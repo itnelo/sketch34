3765f7060d2c0a80080039f5bfecc5b75136872f
Pavel Petrov <itnelo@gmail.com>
2019-06-06 16:13:57 +0300

    migration for entity news record

---
+++ src/Migrations/Version20190606131328.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190606131328 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE app_news (
+          id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+          title VARCHAR(255) NOT NULL, 
+          text CLOB NOT NULL, 
+          synchronization_date DATETIME NOT NULL
+        )');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE app_news');
+    }
+}
